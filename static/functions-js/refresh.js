1273fb18e0686066e873d1f3df56f26717c1fffc
kirk.montejo <kirk.montejo@cit.edu>
2023-11-30 15:43:19 +0800

    Additional for API
    Pushed Lates API file

--- static/functions-js/refresh.js
+++
@@ -1,9 +0,0 @@
-function getUpdates(){
-    fetch('area_count')
-    .then(response => response.json())
-    
-}
-
-setInterval(() => {
-    $("#facility-map").load(" #facility-map > *");
-}, 2000);
