e159b2046ff449f16e5a64551dc9a9337cab0e64
Corazeus <ruizrenzeus@gmail.com>
2023-12-13 02:47:36 +0800

    updated refresh.js

--- wiladmin/static/functions-js/refresh.js
+++ wiladmin/static/functions-js/refresh.js
@@ -21,8 +21,8 @@ setInterval(() => {
             a5.value=JSON.stringify(response.area_count.countA5) + "/6";
             a6.value=JSON.stringify(response.area_count.countA6) + "/6";
             a7.value=JSON.stringify(response.area_count.countA7) + "/24";
-            a8.value=JSON.stringify(response.area_count.countA8) + "/5";
-            a9.value=JSON.stringify(response.area_count.countA9) + "/5";
+            a8.value=JSON.stringify(response.area_count.countA8) + "/6";
+            a9.value=JSON.stringify(response.area_count.countA9) + "/6";
         },
         error: function(error){
             console.log('error', error);
